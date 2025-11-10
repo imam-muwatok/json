7a1cdfd0dd54b8fa9f420126a7839c5ba0576b64
Imam Muwatok <45243273+imam-muwatok@users.noreply.github.com>
2025-11-10 15:56:38 +0000

    Add files via upload

---
+++ project/new_wallet/data/tags.js
@@ -0,0 +1,12 @@
+ const defaultTags = [
+    "Rokok", 
+    "Liquid", 
+    "Kuota", 
+    "Transportasi", 
+    "Gaji", 
+    "Motor", 
+    "Tagihan", 
+    "Bansos", 
+    "Bayar Tagihan Orang", 
+    "Lainnya"
+]
